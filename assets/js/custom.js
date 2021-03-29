feafff57214346b52c24e76e76668e327196e174
Md. Rabiul Alam <alamrobiduet074001@gmail.com>
2021-03-29 12:22:51 +0600

    custom js are Updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
     // let nested6 = document.querySelector(".nested6");
     // let allnested1 = document.querySelector(".allnested1");
     // let allnested3 = document.querySelector(".allnested3");
+    
 
     // let arrow1 = document.querySelector(".arrow1");
     // let arrow2 = document.querySelector(".arrow2");
@@ -99,59 +100,53 @@ $(document).ready(function() {
     // })
  
     
-    $(document).ready(function(){
-      $(".dropdown-submenu a.hasSubMenu").click(function(){
-        $(".dropdown-submenu").toggleClass("subMenuOn");
-      });
-      $('.dropdown-submenu a.hasSubMenu').on("click", function(e){
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-      });
-    });
+    // $(document).ready(function(){
+    //   $(".dropdown-submenu a.hasSubMenu").click(function(){
+    //     $(".dropdown-submenu").toggleClass("subMenuOn");
+    //   });
+    //   $('.dropdown-submenu a.hasSubMenu').on("click", function(e){
+    //     $(this).next('ul').toggle();
+    //     e.stopPropagation();
+    //     e.preventDefault();
+    //   });
+    // });
 
 
-    // Chart Script 
-
-    google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawBasic);
-
-function drawBasic() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number', 'Motivation Level');
-
-      data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1],
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
-        [{v: [13, 0, 0], f: '1 pm'}, 6],
-        [{v: [14, 0, 0], f: '2 pm'}, 7],
-        [{v: [15, 0, 0], f: '3 pm'}, 8],
-        [{v: [16, 0, 0], f: '4 pm'}, 9],
-        [{v: [17, 0, 0], f: '5 pm'}, 10],
-      ]);
-
-      var options = {
-        title: 'Motivation Level Throughout the Day',
-        hAxis: {
-          title: 'Time of Day',
-          format: 'h:mm a',
-          viewWindow: {
-            min: [7, 30, 0],
-            max: [17, 30, 0]
-          }
-        },
-        vAxis: {
-          title: 'Rating (scale of 1-10)'
+    // Chart Script Start Here
+    google.charts.load('current', {packages: ['corechart', 'line']});
+    google.charts.setOnLoadCallback(drawBasic);
+    
+    function drawBasic() {
+    
+          var data = new google.visualization.DataTable();
+          data.addColumn('number', 'X');
+          data.addColumn('number', 'Dogs');
+    
+          data.addRows([
+            [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+            [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+            [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+            [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+            [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+            [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+            [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+            [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+            [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+            [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+            [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+            [66, 70], [67, 72], [68, 75], [69, 80]
+          ]);
+    
+          var options = {
+            hAxis: {
+              title: 'Time'
+            },
+            vAxis: {
+              title: 'Popularity'
+            }
+          };
+    
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    
+          chart.draw(data, options);
         }
-      };
-
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
